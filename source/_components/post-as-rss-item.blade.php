a36e152923065ab8083afa1af3d9fd694b9d5eaf
Kenny Horna <kennyhorna@gmail.com>
2019-11-04 22:33:02 -0500

    Updated: Newsletter activated.

--- source/_components/post-as-rss-item.blade.php
+++ source/_components/post-as-rss-item.blade.php
@@ -2,6 +2,11 @@
     <id>{{ $entry->getUrl() }}</id>
     <link type="text/html" rel="alternate" href="{{ $entry->getUrl() }}" />
     <title>{{ $entry->title }}</title>
+    <image>
+      <title>{{ $entry->title }}</title>
+      <url>{{ $entry->cover_image }}</url>
+      <link>{{ $entry->getUrl() }}</link>
+    </image>
     <published>{{ date(DATE_ATOM, $entry->date) }}</published>
     <updated>{{ date(DATE_ATOM, $entry->date) }}</updated>
     <author>
--- source/index.blade.php
+++ source/index.blade.php
@@ -34,7 +34,7 @@
     @endif
   @endforeach
 
-  {{--    @include('_components.newsletter-signup')--}}
+  @include('_components.newsletter-signup')
 
   @foreach ($posts->where('featured', false)->take(6)->chunk(2) as $row)
     <div class="flex flex-col md:flex-row md:-mx-6">
