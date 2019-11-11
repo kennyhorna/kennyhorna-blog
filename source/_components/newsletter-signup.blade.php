01479da4ec12eb8d48d71211316f34adcf717b92
Kenny Horna <kennyhorna@gmail.com>
2019-11-11 16:17:36 -0500

    Added: "Edit on Github" button.

--- source/_components/newsletter-signup.blade.php
+++ source/_components/newsletter-signup.blade.php
@@ -18,12 +18,6 @@
           <input type="email" value="" name="EMAIL" class="required email " id="mce-EMAIL" placeholder="Email"
                  oninvalid="this.setCustomValidity('El e-mail es requerido.')">
         </div>
-        <div id="mce-responses" class="clear">
-          <div class="response" id="mce-error-response" style="display:none"></div>
-          <div class="response" id="mce-success-response" style="display:none"></div>
-        </div>
-        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-
         <div style="position: absolute; left: -5000px;" aria-hidden="true" class="flex flex-col md:block">
           <input type="text" name="b_25582686a9fc051afd5453557_189578c854" tabindex="-1" value="">
         </div>
@@ -31,6 +25,11 @@
           <input type="submit" value="Únete" name="subscribe" id="mc-embedded-subscribe" class="button">
         </div>
       </div>
+      <div id="mce-responses" class="clear">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none"></div>
+      </div>
+      <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     </form>
   </div>
   <!--End Mailchimp Signup Form -->
