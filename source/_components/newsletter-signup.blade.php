f95ece8f5db65f3a720a70d49d4cded7fc1aece1
Kenny Horna <kennyhorna@gmail.com>
2019-12-30 20:41:04 -0500

    Updated: Mailchimp's JS imported.

--- source/_components/newsletter-signup.blade.php
+++ source/_components/newsletter-signup.blade.php
@@ -46,7 +46,7 @@
 </div>
 
 @push('scripts')
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+<script src="{{ mix('js/local-mc-validate.js', 'assets/build') }}"></script>
 <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday'; /*
  * Translated default messages for the $ validation plugin.
  * Locale: ES
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,30 +6,31 @@ require('laravel-mix-purgecss');
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build/');
 mix.webpackConfig({
-    plugins: [
-        build.jigsaw,
-        build.browserSync('kennyhorna.test'),
-        build.watch([
-            'config.php',
-            'source/**/*.md',
-            'source/**/*.php',
-            'source/**/*.scss',
-        ]),
-    ],
+  plugins: [
+    build.jigsaw,
+    build.browserSync('kennyhorna.test'),
+    build.watch([
+      'config.php',
+      'source/**/*.md',
+      'source/**/*.php',
+      'source/**/*.scss',
+    ]),
+  ],
 });
 
 mix.js('source/_assets/js/main.js', 'js')
-    .sourceMaps()
-    .sass('source/_assets/sass/main.scss', 'css/main.css')
-    .copy('node_modules/share-buttons/dist/share-buttons.js', 'js/share-buttons.js')
-    .sourceMaps()
-    .options({
-        processCssUrls: false,
-        postCss: [tailwindcss()],
-    })
-    .purgeCss({
-        extensions: ['html', 'md', 'js', 'php', 'vue'],
-        folders: ['source'],
-        whitelistPatterns: [/language/, /hljs/],
-    })
-    .version();
+  .copy('source/_assets/js/local-mc-validate.js', 'js')
+  .sourceMaps()
+  .sass('source/_assets/sass/main.scss', 'css/main.css')
+  .copy('node_modules/share-buttons/dist/share-buttons.js', 'js/share-buttons.js')
+  .sourceMaps()
+  .options({
+    processCssUrls: false,
+    postCss: [tailwindcss()],
+  })
+  .purgeCss({
+    extensions: ['html', 'md', 'js', 'php', 'vue'],
+    folders: ['source'],
+    whitelistPatterns: [/language/, /hljs/],
+  })
+  .version();
